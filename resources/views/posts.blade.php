95644069762bc630fb10369ebc7c73fc44e5ee96
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-31 14:13:55 +0100

    initialize git repository

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Blog</title>
+  <link rel="stylesheet" href="./app.css" type="text/css">
+</head>
+
+<body>
+  <?php foreach($posts as $post): ?>
+    <h1>
+      <?= $post; ?>
+    </h1>
+    <?php endforeach; ?>
+</body>
+
+</html>
